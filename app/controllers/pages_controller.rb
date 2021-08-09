102a0de61fc42603ed372c81609f032158c3b36f
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-08-09 13:13:20 +0800

    0809 WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def main
-    # render html: 123
+    @abc = "Welcome"
+  end
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do 
+  get "/notes", to: "notes#index"
+
   get "/hello", to: "pages#main"
+  get "/about", to: "pages#about"
+  get "/users", to: "users#profile"
 end
