9446359c0a68b518832fa63c6d646013b3ee420a
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-08-10 17:09:37 +0800

    add notes list func

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,23 +1,25 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.order(id: :desc)
   end
 
   def new
     @note = Note.new  
   end
 
+  def show
+    @note = Note.find(params[:id])
+  end  
+
   def create
-
-    title = params[:title]
-    content = params[:content]
-    note = Note.new(title: title, content: content)
-
-    if note.save
+    clean_params = params.require(:note).permit(:title, :content)
+    @note = Note.new(clean_params)
+    
+    if @note.save
       redirect_to "/notes"
     else
-      
+      render :new
     end
-
   end
 end
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  validates :title, presence: true #validates 是類別方法
+  validates :content, presence: true
 end
