dd841171b7f2c4afcd72bc3b4e8291ad34df7e0a
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-08-12 23:37:47 +0800

    complete CRUB

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,25 +1,48 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: %i[show edit update destroy]
+
   def index
     @notes = Note.order(id: :desc)
   end
 
+  def show; end
+
   def new
-    @note = Note.new  
+    @note = Note.new
   end
 
-  def show
-    @note = Note.find(params[:id])
-  end  
-
   def create
-    clean_params = params.require(:note).permit(:title, :content)
     @note = Note.new(clean_params)
-    
+
     if @note.save
-      redirect_to "/notes"
+      redirect_to '/notes'
     else
       render :new
     end
   end
-end
 
+  def edit; end
+
+  def update
+    if @note.update(clean_params)
+      redirect_to '/notes'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to '/notes'
+  end
+
+  private
+
+  def clean_params
+    params.require(:note).permit(:title, :content)
+  end
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-Rails.application.routes.draw do   
+Rails.application.routes.draw do
   resources :notes
 
-
-  get "/hello", to: "pages#main"
-  get "/about", to: "pages#about"
-  get "/users", to: "users#profile"
+  get '/hello', to: 'pages#main'
+  get '/about', to: 'pages#about'
+  get '/users', to: 'users#profile'
 end
